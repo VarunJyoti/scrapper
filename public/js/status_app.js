a88fbfd3d5f3032e78b76593bc1d78a23132c607
Varun Jyoti <Varun.jyoti@basware.com>
2017-03-05 12:40:00 +0530

    Ijtroducing URL routing

---
+++ public/js/status_app.js
@@ -0,0 +1,26 @@
+
+
+ $(document).ready(function(){
+    function viewModel() {
+    var self = this;
+    self.currentView = ko.observable();
+    self.views = ko.observableArray(["Running", "PNR", "Cancelled"]);
+    self.show_Contact = ko.computed(function() {
+        return self.currentView() === "Running" ? true : false;
+    });
+    self.show_Home = ko.computed(function() {
+        return self.currentView() === "PNR" ? true : false;  
+    });
+    self.show_About = ko.computed(function() {
+        return self.currentView() === "Cancelled" ? true : false;
+    });    
+}
+
+var vm = new viewModel();
+ko.applyBindings(vm, document.getElementById("root"));
+Sammy(function () {
+        this.get('#:view', function () {
+            vm.currentView(this.params.view);
+        });
+    }).run('#Home');
+ })
