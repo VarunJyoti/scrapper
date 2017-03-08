bdeae3c219ee05e12c6450ada5e32e765715e362
Anil Kumar Goyal <Anil.Kumar.Goyal@basware.com>
2017-03-08 22:26:02 +0530

    current Location of trai shown by icon

--- public/js/status_app.js
+++ public/js/status_app.js
@@ -1,25 +1,54 @@
 
  $(document).ready(function(){
     function viewModel() {
-        var self = this;
-        self.currentView = ko.observable();
-        self.views = ko.observableArray(["Running Status", "PNR Status","Diverted Trains", "Rescheduled Trains", "Cancelled Trains"]);
-        self.show_Contact = ko.computed(function() {
-            return self.currentView() === "Running Status" ? true : false;
+        var model = {
+            currentView : ko.observable(),
+            trainNumber : ko.observable(""),
+            currentPosition : ko.observable(0),
+            enableGetRunningStatus: ko.observable(false)
+
+        };
+
+        model.views = ko.observableArray(["Running Status", "PNR Status","Diverted Trains", "Rescheduled Trains", "Cancelled Trains"]);
+        model.show_Contact = ko.computed(function() {
+            return model.currentView() === "Running Status" ? true : false;
         });
-        self.show_Home = ko.computed(function() {
-            return self.currentView() === "PNR Status" ? true : false;
+        model.show_Home = ko.computed(function() {
+            return model.currentView() === "PNR Status" ? true : false;
         });
-        self.show_About = ko.computed(function() {
-            return self.currentView() === "Diverted Trains" ? true : false;
+        model.show_About = ko.computed(function() {
+            return model.currentView() === "Diverted Trains" ? true : false;
         });
-        self.show_About = ko.computed(function() {
-            return self.currentView() === "Rescheduled Trains" ? true : false;
+        model.show_About = ko.computed(function() {
+            return model.currentView() === "Rescheduled Trains" ? true : false;
         });
-        self.show_About = ko.computed(function() {
-            return self.currentView() === "Cancelled Trains" ? true : false;
+        model.show_About = ko.computed(function() {
+            return model.currentView() === "Cancelled Trains" ? true : false;
         });
+
+        model.enableGetRunningStatus = ko.computed(function () {
+            return (model.trainNumber().length>0);
+        });
+
+        model.getRunningStatus = getRunningStatus.bind(model)
+
+        return model;
     }
+
+     function getRunningStatus(model) {
+        if(isNaN(model.trainNumber()))
+        {
+            alert("enter valid train number");
+            return;
+        }
+         // 70/1400 *50px = x
+         var cp = model.trainNumber() / 1400 * 350;
+         cp = cp.toFixed(2)
+        /* if (cp > 335) {
+             cp = 335
+         }*/
+         model.currentPosition(cp)
+     }
 
     var vm = new viewModel();
     ko.applyBindings(vm, document.getElementById("root"));
