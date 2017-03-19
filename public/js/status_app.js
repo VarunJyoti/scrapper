80dfed2b0d64e2adf5b6833a45468ff2b3414c0e
Anil Kumar Goyal <Anil.Kumar.Goyal@basware.com>
2017-03-19 12:16:23 +0530

    button removed and ajax call done for error message and char length 5

--- public/js/status_app.js
+++ public/js/status_app.js
@@ -55,23 +55,41 @@ $(document).ready(function () {
         };
         model.views = ko.observableArray(["Running Status", "Diverted Trains", "Rescheduled Trains", "Cancelled Trains"]);
 
-        /* model.show_Contact = ko.computed(function () {
-         return model.currentView() === "Running Status" ? true : false;
+       /* model.show_Contact = ko.computed(function () {
+            return model.currentView() === "Running Status" ? true : false;
 
-         });
-         model.show_Home = ko.computed(function () {
-         return model.currentView() === "PNR Status" ? true : false;
-         });
-         model.show_About = ko.computed(function () {
-         return model.currentView() === "Diverted Trains" ? true : false;
-         });
-         model.show_About = ko.computed(function () {
-         return model.currentView() === "Rescheduled Trains" ? true : false;
-         });
-         model.show_About = ko.computed(function () {
-         return model.currentView() === "Cancelled Trains" ? true : false;
-         });
-         */
+        });
+        model.show_Home = ko.computed(function () {
+            return model.currentView() === "PNR Status" ? true : false;
+        });
+        model.show_About = ko.computed(function () {
+            return model.currentView() === "Diverted Trains" ? true : false;
+        });
+        model.show_About = ko.computed(function () {
+            return model.currentView() === "Rescheduled Trains" ? true : false;
+        });
+        model.show_About = ko.computed(function () {
+            return model.currentView() === "Cancelled Trains" ? true : false;
+        });*/
+
+        ko.computed(function () {
+            var cv = model.currentView();
+            switch (cv) {
+                case "Diverted Trains":
+                    getDivertedTrains(model);
+                    break;
+                case "Rescheduled Trains":
+                    getRescheduledTrains(model);
+                    break;
+                case "Cancelled Trains":
+                    getCancelledTrains(model);
+                    break
+
+                default:
+                    break;
+            }
+        });
+
 
         model.runningStatusModel.enableGetRunningStatus = ko.computed(function () {
             return (model.runningStatusModel.runningTrainNo().length > 0);
@@ -107,18 +125,23 @@ $(document).ready(function () {
     function getPNRStatus(m){
         var model = m.pnrModel;
     }
+
     function getRunningStatus(m) {
         var model = m.runningStatusModel;
         $("#overlay").show();
-        $.get("/scrapping/" + model.runningTrainNo(), function (data) {
+        ajaxCall("/scrapping/" + model.runningTrainNo(),
+            function (data) {
                 $("#overlay").hide();
                 runningData = data[0];
                 model.rakes(runningData.rakes);
                 model.trainName(model.runningTrainNo() + " " + data[0].trainName);
                 var rake = runningData.rakes[0];
                 setRakeBasedData(rake, model);
-            }
-        )
+            },
+            function () {
+                alert("Train number incorrect or Server too busy. Please try after some time.");
+                $("#overlay").hide();
+            });
     }
 
     function setRakeBasedData(rake, model) {
@@ -176,7 +199,7 @@ $(document).ready(function () {
     function getCancelledTrains(m) {
         m.cancelledTrains([])
         $("#overlay").show();
-        $.get("/getCancelled", function (data) {
+        ajaxCall("/getCancelled", function (data) {
             $("#overlay").hide();
             data.allCancelledTrains.forEach(function (ct) {
                 var t = {
@@ -189,13 +212,13 @@ $(document).ready(function () {
                 }
                 m.cancelledTrains.push(t);
             })
-        });
+        }, serverBusy);
     }
 
     function getDivertedTrains(m) {
         m.divertedTrains([]);
         $("#overlay").show();
-        $.get("/getDiverted", function (data) {
+        ajaxCall("/getDiverted", function (data) {
             $("#overlay").hide();
             data.trains.forEach(function (ct) {
                 var t = {
@@ -210,32 +233,47 @@ $(document).ready(function () {
                 }
                 m.divertedTrains.push(t);
             })
-        })
+        }, serverBusy)
     }
 
     function getRescheduledTrains(m) {
         m.rescheduledTrains([]);
         $("#overlay").show();
-        $.get("/getRescheduled", function (data) {
-            $("#overlay").hide();
-            data.trains.forEach(function (ct) {
-                var t = {
-                    startDate: ct.startDate,
-                    trainDstn: ct.trainDstn,
-                    trainName: ct.trainName,
-                    trainNo: ct.trainNo,
-                    trainSrc: ct.trainSrc,
-                    trainType: ct.trainType,
-                    startDate: ct.startDate,
-                    newStartDate: ct.newStartDate,
-                    actDep: ct.actDep,
-                    schDep: ct.schDep,
-
-                }
-                m.rescheduledTrains.push(t);
-            })
-        });
+        ajaxCall("/getRescheduled",
+            function (data) {
+                $("#overlay").hide();
+                data.trains.forEach(function (ct) {
+                    var t = {
+                        startDate: ct.startDate,
+                        trainDstn: ct.trainDstn,
+                        trainName: ct.trainName,
+                        trainNo: ct.trainNo,
+                        trainSrc: ct.trainSrc,
+                        trainType: ct.trainType,
+                        startDate: ct.startDate,
+                        newStartDate: ct.newStartDate,
+                        actDep: ct.actDep,
+                        schDep: ct.schDep
+                    }
+                    m.rescheduledTrains.push(t);
+                })
+            }, serverBusy);
     }
+
+    var serverBusy = function () {
+        alert("Server too busy. Please try after some time.");
+        $("#overlay").hide();
+    }
+
+    function ajaxCall(url, success, failure) {
+        $.ajax({
+            url: url,
+            success: success,
+            error: failure,
+            timeout: 9000
+        })
+    }
+
 
     var vm = new viewModel();
 
